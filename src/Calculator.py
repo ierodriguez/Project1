5424d0fd5f17f18669eca35f7a4419dec7adeee0
ierodriguez <ier2@njit.edu>
2021-06-29 02:02:27 -0400

    Feature: Setup calculator functions (division, square, square root, multiplication, subtraction, addition) with tests.

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,3 +1,31 @@
+import math
+
+
+def addition(a, b):
+    return float(a) + float(b)
+
+
+def subtraction(a, b):
+    return float(a) - float(b)
+
+
+def multiplication(a, b):
+    return float(a) * float(b)
+
+
+def division(a, b):
+    if int(b) != 0:
+        return float(a) / float(b)
+    else:
+        return ZeroDivisionError
+
+
+def square(a):
+    return float(a) ** 2
+
+
+def squareroot(a):
+    return math.sqrt(float(a))
 
 class Calculator:
     result = 0
@@ -5,3 +33,26 @@ class Calculator:
     def __init__(self):
         pass
 
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def divide(self, a, b):
+        self.result = division(a, b)
+        return self.result
+
+    def square(self, a):
+        self.result = square(a)
+        return self.result
+
+    def square_root(self, a):
+        self.result = squareroot(a)
+        return self.result
